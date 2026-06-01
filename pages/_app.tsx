973c10e09e614d2712363a21191b977e081dc024
wdenning <73503493+wdenning@users.noreply.github.com>
2026-06-01 11:11:50 -0400

    metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,19 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>PCT Hike Log</title>
+        <meta name="description" content="Summer 2026 flip-flop thru-hike of the Pacific Crest Trail, starting at Echo Lake near South Lake Tahoe." />
+        <meta name="author" content="William Denning" />
+        <meta property="og:title" content="PCT Hike Log" />
+        <meta property="og:description" content="Summer 2026 flip-flop thru-hike of the Pacific Crest Trail." />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="PCT Hike Log" />
+        <meta name="twitter:description" content="Summer 2026 flip-flop thru-hike of the Pacific Crest Trail." />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🥾</text></svg>"
+        />
       </Head>
       <Component {...pageProps} />
     </>
